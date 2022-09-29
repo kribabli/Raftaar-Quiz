2461db4e0abaed2b24a0b624feab3bde5444479e
Mt Software Solutions <95462784+mtteam21@users.noreply.github.com>
2022-09-29 16:55:39 +0530

    HomeFragment

---
+++ app/src/main/java/com/example/raftaarquiz/LoginModule/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.raftaarquiz.LoginModule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.raftaarquiz.R;
+
+public class LoginActivity extends AppCompatActivity {
+    TextView signUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        signUp = findViewById(R.id.signUp);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/raftaarquiz/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.example.raftaarquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.raftaarquiz.LoginModule.SignUpActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+public class SplashActivity extends AppCompatActivity {
+    Handler handler;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+        GoogleSignInAccount googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this);
+
+        handler = new Handler();
+        handler.postDelayed(() -> {
+            if (googleSignInAccount != null) {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Intent intent = new Intent(SplashActivity.this, SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
