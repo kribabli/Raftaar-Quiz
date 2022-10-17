70d672416cec553ac732b27313d565e3e6b92bc6
vikipatel-gdd <71511984+vikipatel-gdd@users.noreply.github.com>
2022-10-17 15:49:53 +0530

    code commited

--- app/src/main/java/com/example/raftaarquiz/AllActivity/QuizActivity.java
+++ app/src/main/java/com/example/raftaarquiz/AllActivity/QuizActivity.java
@@ -16,6 +16,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.raftaarquiz.AllAdapter.QuizAdapter;
+import com.example.raftaarquiz.AllAdapter.Tournament_quizAdapter;
 import com.example.raftaarquiz.Model.QuizCategories;
 import com.example.raftaarquiz.R;
 
@@ -29,27 +30,76 @@ import java.util.Map;
 
 public class QuizActivity extends AppCompatActivity {
     RecyclerView recyclerView;
+    RecyclerView tournament_Quiz;
     SwipeRefreshLayout swiper;
     QuizAdapter quizAdapter;
     String quizUrl = "https://adminapp.tech/raftarquiz/api/exam/categories";
     ArrayList<QuizCategories> listItems = new ArrayList<>();
     TextView backPress;
+    Tournament_quizAdapter tournament_quizAdapter;
+    ArrayList<QuizCategories> tournament_listItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         getAllQuizList();
+        getAllTournamentList();
         initMethod();
         setAction();
     }
 
+    private void getAllTournamentList() {
+        tournament_listItems.clear();
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, quizUrl, response -> {
+            try {
+                tournament_listItems.clear();
+                JSONObject jsonObject = new JSONObject(response);
+                JSONArray jsonArray = jsonObject.getJSONArray("data");
+                try {
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+                        String id = jsonObject1.getString("id");
+                        String title = jsonObject1.getString("title");
+                        String image = jsonObject1.getString("image");
+//                        String open_time = jsonObject1.getString("open_time");
+//                        String close_time = jsonObject1.getString("close_time");
+//                        String date = jsonObject1.getString("date");
+                        QuizCategories quizCategories = new QuizCategories(id, title, image,"","","");
+                        tournament_listItems.add(quizCategories);
+                    }
+                    tournament_Quiz.setLayoutManager(new GridLayoutManager(this, 2));
+                    tournament_quizAdapter = new Tournament_quizAdapter(tournament_listItems);
+                    tournament_Quiz.setAdapter(tournament_quizAdapter);
+                    tournament_quizAdapter.notifyDataSetChanged();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }, error -> {
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                return params;
+            }
+        };
+        queue.add(stringRequest);
+
+
+    }
+
     private void initMethod() {
         recyclerView = findViewById(R.id.recycler);
+        tournament_Quiz = findViewById(R.id.tournament_Quiz);
         swiper = findViewById(R.id.swiper);
         backPress = findViewById(R.id.backPress);
         Animation animSlideDown = AnimationUtils.loadAnimation(this,R.anim.slide_down);
         recyclerView.startAnimation(animSlideDown);
+        tournament_Quiz.startAnimation(animSlideDown);
     }
 
     private void setAction() {
@@ -58,8 +108,10 @@ public class QuizActivity extends AppCompatActivity {
             public void onRefresh() {
                 swiper.setRefreshing(false);
                 getAllQuizList();
+                getAllTournamentList();
                 Animation animSlideDown = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_down);
                 recyclerView.startAnimation(animSlideDown);
+                tournament_Quiz.startAnimation(animSlideDown);
             }
         });
 
@@ -85,10 +137,7 @@ public class QuizActivity extends AppCompatActivity {
                         String id = jsonObject1.getString("id");
                         String title = jsonObject1.getString("title");
                         String image = jsonObject1.getString("image");
-                        String open_time = jsonObject1.getString("open_time");
-                        String close_time = jsonObject1.getString("close_time");
-                        String date = jsonObject1.getString("date");
-                        QuizCategories quizCategories = new QuizCategories(id, title, image,open_time,close_time,date);
+                        QuizCategories quizCategories = new QuizCategories(id, title, image,"","","");
                         listItems.add(quizCategories);
                     }
                     recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
