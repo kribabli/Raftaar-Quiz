c217325071d45daf6281f30f10372d4559e6fdd4
AppSync-ai <81430894+AppSync-ai@users.noreply.github.com>
2022-11-05 18:58:31 +0530

    MockTestActivity.java

--- app/src/main/java/com/example/raftaarquiz/AllActivity/AvailableTestSeriesActivity.java
+++ app/src/main/java/com/example/raftaarquiz/AllActivity/AvailableTestSeriesActivity.java
@@ -6,12 +6,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -63,6 +66,8 @@ public class AvailableTestSeriesActivity extends AppCompatActivity {
 
     private void setAction() {
         backPress.setOnClickListener(view -> onBackPressed());
+
+        swipeRefreshLayout.setOnRefreshListener(() -> getAvailableTestSeriesList());
     }
 
     private void getAvailableTestSeriesList() {
@@ -98,6 +103,7 @@ public class AvailableTestSeriesActivity extends AppCompatActivity {
                         availableTestSeriesAdapter = new AvailableTestSeriesAdapter(list, getApplicationContext());
                         recyclerView.setAdapter(availableTestSeriesAdapter);
                         availableTestSeriesAdapter.notifyDataSetChanged();
+                        swipeRefreshLayout.setRefreshing(false);
                     } catch (JSONException e) {
                         swipeRefreshLayout.setRefreshing(false);
                         e.printStackTrace();
@@ -134,7 +140,7 @@ public class AvailableTestSeriesActivity extends AppCompatActivity {
         }
 
         @Override
-        public void onBindViewHolder(@NonNull AvailableTestSeriesAdapter.ViewHolder holder, int position) {
+        public void onBindViewHolder(@NonNull AvailableTestSeriesAdapter.ViewHolder holder, @SuppressLint("RecyclerView") int position) {
             holder.setIsRecyclable(false);
             holder.testSeriesTitle.setText("" + list.get(position).getTitle());
             holder.noOfTest.setText("" + list.get(position).getTotal() + " Total tests");
@@ -144,6 +150,17 @@ public class AvailableTestSeriesActivity extends AppCompatActivity {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+
+            holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(context, MockTestActivity.class);
+                    intent.putExtra("id", list.get(position).getId());
+                    intent.putExtra("img", list.get(position).getImage());
+                    intent.putExtra("title", list.get(position).getTitle());
+                    context.startActivity(intent);
+                }
+            });
         }
 
         @Override
@@ -154,12 +171,14 @@ public class AvailableTestSeriesActivity extends AppCompatActivity {
         public class ViewHolder extends RecyclerView.ViewHolder {
             CircleImageView circleImageView;
             TextView testSeriesTitle, noOfTest;
+            RelativeLayout relativeLayout;
 
             public ViewHolder(@NonNull View itemView) {
                 super(itemView);
                 circleImageView = itemView.findViewById(R.id.testSeriesImg);
                 testSeriesTitle = itemView.findViewById(R.id.testSeriesTitle);
                 noOfTest = itemView.findViewById(R.id.noOfTest);
+                relativeLayout = itemView.findViewById(R.id.parentRelative);
             }
         }
     }
---
+++ app/src/main/java/com/example/raftaarquiz/AllActivity/MockTestActivity.java
@@ -0,0 +1,16 @@
+package com.example.raftaarquiz.AllActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.raftaarquiz.R;
+
+public class MockTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mock_test);
+    }
+}
